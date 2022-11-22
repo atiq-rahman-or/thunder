5f72e6215d21f39b8065fd1b1fe8beaa5072c140
icrforjava <icrforjava@openrefactory.com>
2022-11-22 03:04:02 +0000

    Fix Null Dereference Check
    In file: LNPaymentProcessorImpl.java, class: LNPaymentProcessorImpl, method: updatePaymentsDatabase there is a potential Null pointer dereference. This may throw an unexpected null pointer exception which, if unhandled, may crash the program. iCR detected the null pointer issue and demonstrated the full path from the object declaration to the null dereference in the object. A developer should introduce null checks in the appropriate path or initialize the object explicitly.

--- thunder-core/src/main/java/network/thunder/core/communication/layer/high/payments/LNPaymentProcessorImpl.java
+++ thunder-core/src/main/java/network/thunder/core/communication/layer/high/payments/LNPaymentProcessorImpl.java
@@ -349,7 +349,18 @@ public class LNPaymentProcessorImpl extends LNPaymentProcessor {
                 wrapper.statusReceiver = REDEEMED;
                 dbHandler.updatePaymentReceiver(wrapper);
             } else {
-                wrapper.statusSender = REDEEMED;
+                
+				/* ********OpenRefactory Warning********
+				 Possible null pointer Dereference!
+				 Path: 
+					File: LNPaymentProcessorImpl.java, Line: 347
+						PaymentWrapper wrapper=dbHandler.getPayment(payment.secret);
+						Variable wrapper is initialized null.
+					File: LNPaymentProcessorImpl.java, Line: 352
+						wrapper.statusSender=REDEEMED;
+						wrapper is referenced in field access.
+				*/
+				wrapper.statusSender = REDEEMED;
                 dbHandler.updatePaymentSender(wrapper);
             }
             eventHelper.onPaymentCompleted(payment);
